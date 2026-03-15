3d02eca26e0627bf7b3214c7a9bded7d0de1bfa9
woodrow <woodrowjamesgamboa@gmail.com>
2026-03-15 11:02:21 -0500

    Added argoarse to tokenizer

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,11 +1,28 @@
 import tiktoken
+import argparse
 
-tokenizer = tiktoken.get_encoding("gpt2")
+def tokenize_file(filename):
+    tokenizer = tiktoken.get_encoding("gpt2")
 
-text = "Hello, edited world!"
-tokens = tokenizer.encode(text)
-text_decoded = tokenizer.decode(tokens)
-text_decoded_literal = [ tokenizer.decode([token]) for token in tokens ]
-print("Tokens:", tokens)
-print("Decoded tokens:", text_decoded_literal)
-print("Decoded text:", text_decoded)
+    with open(filename, "r", encoding="utf-8") as f:
+        text = f.read()
+
+    tokens = tokenizer.encode(text)
+    text_decoded = tokenizer.decode(tokens)
+    text_decoded_literal = [tokenizer.decode([token]) for token in tokens]
+
+    print("Tokens:", tokens)
+    print("Decoded tokens:", text_decoded_literal)
+    print("Decoded text:", text_decoded)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+                        prog='tokenizer',
+                        description='tokenizes text file',
+                        epilog='Ask god')
+
+    parser.add_argument('filename')
+    args = parser.parse_args()    
+
+    tokenize_file(args.filename)
